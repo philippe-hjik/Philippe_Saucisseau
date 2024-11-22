175c613acce14ce7b28175d56ee6c470252acb48
philippe-hjik <122774959+philippe-hjik@users.noreply.github.com>
2024-11-22 13:46:43 +0100

    feat(app): send message on mqtt server
    25min-DONE

--- WinFormsSaucisseau/WinFormsSaucisseau/Form1.Designer.cs
+++ WinFormsSaucisseau/WinFormsSaucisseau/Form1.Designer.cs
@@ -29,6 +29,8 @@
         private void InitializeComponent()
         {
             listView1 = new ListView();
+            button1 = new Button();
+            label1 = new Label();
             SuspendLayout();
             // 
             // listView1
@@ -39,20 +41,44 @@
             listView1.TabIndex = 0;
             listView1.UseCompatibleStateImageBehavior = false;
             // 
+            // button1
+            // 
+            button1.Location = new Point(137, 84);
+            button1.Name = "button1";
+            button1.Size = new Size(153, 48);
+            button1.TabIndex = 1;
+            button1.Text = "Je veux les musiques";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click_1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(365, 61);
+            label1.Name = "label1";
+            label1.Size = new Size(38, 15);
+            label1.TabIndex = 2;
+            label1.Text = "label1";
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(label1);
+            Controls.Add(button1);
             Controls.Add(listView1);
             Name = "Form1";
             Text = "Form1";
             Load += Form1_Load;
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private ListView listView1;
+        private Button button1;
+        private Label label1;
     }
 }
--- WinFormsSaucisseau/WinFormsSaucisseau/Form1.cs
+++ WinFormsSaucisseau/WinFormsSaucisseau/Form1.cs
@@ -17,7 +17,7 @@ namespace WinFormsSaucisseau
 
         }
 
-        string broker = "mqtt.blue.section-inf.ch";
+        string broker = "inf-n510-p301";
         int port = 1883;
         string clientId = Guid.NewGuid().ToString();
         string topic = "test";
@@ -68,7 +68,6 @@ namespace WinFormsSaucisseau
 
         public async void creatConnection()
         {
-
             // Create a MQTT client factory
             var factory = new MqttFactory();
 
@@ -98,14 +97,60 @@ namespace WinFormsSaucisseau
                     MessageBox.Show($"Received message: {Encoding.UTF8.GetString(e.ApplicationMessage.PayloadSegment)}");
                     return Task.CompletedTask;
                 };
+
+                // Publish a message 10 times
+                for (int i = 0; i < 1; i++)
+                {
+                    var message = new MqttApplicationMessageBuilder()
+                        .WithTopic(topic)
+                        .WithPayload($"Hello, MQTT! Message number {i}")
+                        .WithQualityOfServiceLevel(MqttQualityOfServiceLevel.AtLeastOnce)
+                        .WithRetainFlag()
+                        .Build();
+
+                    await mqttClient.PublishAsync(message);
+                    await Task.Delay(1000); // Wait for 1 second
+                }
             }
-
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private async void button1_Click_1(object sender, EventArgs e)
         {
+            // Créez un client MQTT
+            var factory = new MqttFactory();
+            var mqttClient = factory.CreateMqttClient();
 
+            // Créez les options de connexion au broker MQTT
+            var options = new MqttClientOptionsBuilder()
+                .WithTcpServer(broker, port)
+                .WithCredentials(username, password)
+                .WithClientId(clientId)
+                .WithCleanSession()
+                .Build();
+
+            // Connexion au broker MQTT
+            var connectResult = await mqttClient.ConnectAsync(options);
+
+            if (connectResult.ResultCode == MqttClientConnectResultCode.Success)
+            {
+                MessageBox.Show("Connected to MQTT broker successfully.");
+
+                // Publier un message
+                var message = new MqttApplicationMessageBuilder()
+                    .WithTopic(topic)
+                    .WithPayload("HELLO, qui a des musiques")
+                    .WithQualityOfServiceLevel(MqttQualityOfServiceLevel.AtLeastOnce)
+                    .WithRetainFlag()
+                    .Build();
+
+                // Publier le message
+                await mqttClient.PublishAsync(message);
+                MessageBox.Show("Message sent successfully!");
+            }
+            else
+            {
+                MessageBox.Show("Failed to connect to MQTT broker.");
+            }
         }
-
     }
 }
