b57afe7f6421066a05e6fef5430e29f3998c9d13
philippe-hjik <122774959+philippe-hjik@users.noreply.github.com>
2024-11-29 14:29:26 +0100

    fix(EnvoiCatalogue): namespace problem

--- WinFormsSaucisseau/WinFormsSaucisseau/Classes/Enveloppes/EnveloppeEnvoieCatalogue.cs
+++ WinFormsSaucisseau/WinFormsSaucisseau/Classes/Enveloppes/EnveloppeEnvoieCatalogue.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using WinFormsSaucisseau.Classes.MediaData;
+using WinFormsSaucisseau;
 
-namespace WinFormsSaucisseau.Classes.Enveloppes
+namespace WinFormsSaucisseau.Classes.MediaData
 {
     public class EnveloppeEnvoieCatalogue
     {
